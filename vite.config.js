aa6e94200fecfb212d8777ba28234b56c209c482
nissouoff <156860993+nissouoff@users.noreply.github.com>
2025-12-17 23:44:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: '.', // racine = dossier où se trouve index.html
-  publicDir: 'public',
+  root: 'src',        // Vite regarde dans src/ pour index.html
+  publicDir: '../public', // dossier public à copier dans build
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: '../dist', // build final à la racine
     emptyOutDir: true,
     rollupOptions: {
-      input: 'index.html', // <-- indique explicitement l’entry point
+      input: 'src/index.html', // explicitement l’entrée
     },
   },
 });
